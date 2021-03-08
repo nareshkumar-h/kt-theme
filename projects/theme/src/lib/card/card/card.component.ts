e805d38d86b474454547d11e454c27ad824934cd
Naresh Kumar H <nareshkumarh@live.com>
2021-03-08 07:32:02 +0530

    added card status

--- projects/theme/src/lib/card/card/card.component.ts
+++ projects/theme/src/lib/card/card/card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'kt-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  @Input()
+  status: string;
 }
